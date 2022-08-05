6e71a689026350add0a6fa8a5f8819d560ac44d3
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-08-05 20:25:00 +0200

    Setup tiptap with ngx-tiptap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'tiptap-test';
+export class AppComponent implements OnDestroy {
+    editor = new Editor({
+        extensions: [StarterKit],
+    });
+
+    value = '<p>Hello, Tiptap!</p>';
+
+    ngOnDestroy(): void {
+        this.editor.destroy();
+
+        this.editor;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxTiptapModule } from 'ngx-tiptap';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, FormsModule, NgxTiptapModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
